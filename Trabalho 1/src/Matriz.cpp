6fa26c161cea0a1ed4b493f82945b8d6730096a9
gabrielamaz <gabriela.azevedo12@gmail.com>
2021-10-23 11:11:11 -0300

    multiplica_por_cte criada

--- Trabalho 1/src/Matriz.cpp
+++ Trabalho 1/src/Matriz.cpp
@@ -11,11 +11,6 @@ void inicializaMatriz(Matriz &X, int ls, int cs, const double elem) {
     X.m = new double * [ls];
     for (int i = 0; i < ls; i++) X.m[i] = new double[cs];
 
-    /*X.m = (double**) malloc(ls * sizeof(double));
-    for (int i = 0; i < cs; i++) {
-        X.m[i] = (double*) malloc(cs * sizeof(double));
-    }*/
-
     for (int i = 0; i < ls; i++) {
         for (int j = 0; j < cs; j++) {
             X.m[i][j] = elem;
@@ -30,11 +25,19 @@ void apagaMatriz(Matriz &X) {
 }
 
 Matriz transposta(Matriz &X) {
+    
+    for(int i = 0; i < X.nLinhas; i++){
+        
+    }
     return X;
 }
 
 Matriz multiplica_por_cte(Matriz &X, double k) {
-
+    for (int i = 0; i < X.nLinhas; i++) {
+        for (int j = 0; j < X.nColunas; j++) {
+            X.m[i][j] *= k;
+        }
+    }
     return X;
 }
 
