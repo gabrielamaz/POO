73116739fd220887e215978b25b0520e580fbc87
Breno Miguel <breno.tecnico@outlook.com>
2021-11-01 19:31:29 -0300

    trabalho 1 parte 2

--- Trabalho 1/Parte 1/src/main.cpp
+++ Trabalho 1/Parte 1/src/main.cpp
@@ -31,19 +31,34 @@ void leLinha()
   arquivo.close();
 }
 
-vector <int> leVetor() {
+void leVetor() {
   int entrada;
   vector <int> numeros;
+  vector <int> quantidade;
   while ((cin >> entrada) && (entrada > 0)) {
     numeros.push_back(entrada);
   }
   sort(numeros.begin(), numeros.end());
+  quantidade = numeros;
   numeros.erase(std::unique(numeros.begin(), numeros.end()), numeros.end());
-  return numeros;
+  for (std::vector<int>::const_iterator i = numeros.begin(); i != numeros.end(); ++i) 
+  {
+    int counter = 0;
+    for(std::vector<int>::const_iterator n = quantidade.begin(); n != quantidade.end(); ++n)
+    {
+      if(*i == *n) counter++;
+    }
+    cout << counter << ' ' << *i << ' ';
+  }
+  cout << endl;
 }
 
 int main()
 {
+  cout << "  * Este programa se refere a atividade 1.1 e 1.2 da Atividade Pratica 01" << endl;
+  cout << "  * Primeiramente ele ira retornar o numero de espacos do arquivo: arquivo.txt" << endl;
+  cout << "  * Em seguida digite a sequencia de numeros desejada" << endl;
   leLinha();
   cout << numeroEspacos << endl;
+  leVetor();
 }
