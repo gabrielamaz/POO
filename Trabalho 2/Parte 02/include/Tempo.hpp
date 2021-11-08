8726629c0fbef1d588e457f5683651a1da1d258a
gabrielamaz <gabriela.azevedo12@gmail.com>
2021-11-08 14:58:49 -0300

    começando a escrever funçoes da classe Tempo

--- Trabalho 2/Parte 02/include/Tempo.hpp
+++ Trabalho 2/Parte 02/include/Tempo.hpp
@@ -2,19 +2,43 @@
 #define TEMPO_HPP
 
 class Tempo {
-  public:
-    Tempo(int dia = 0, int hora = 0, int min = 0, int seg = 0);
-    void verificaConsistencia();
-    Tempo somaTempo(Tempo t1);
-    void leituraArquivo();
-    void escritaArquivo();
-    void incrementaSegundo();
-
  private:
   int dia;
   int hora;
   int min;
   int seg;
+
+  void verificaSegundos();
+  void verificaMinutos();
+  void verificaHoras();
+
+ public:
+  Tempo(int dia = 0, int hora = 0, int min = 0, int seg = 0);
+
+  int getSeg() { return seg; }
+
+  int getMin() { return min; }
+
+  int getHora() { return hora; }
+
+  int getDia() { return dia; }
+
+  void setSeg(int seg) { this->seg = seg; }
+
+  void setMin(int min) { this->min = min; }
+
+  void setHora(int hora) { this->hora = hora; }
+
+  void setDia(int dia) { this->dia = dia; }
+
+  void verificaConsistencia();
+  Tempo somaTempo(Tempo t1);
+  void leituraArquivo();
+  void escritaArquivo();
+  void incrementaSegundo();
+  void incrementaMinuto();
+  void incrementaHora();
+  void incrementaDia();
 };
 
 #endif
--- Trabalho 2/Parte 02/src/Tempo.cpp
+++ Trabalho 2/Parte 02/src/Tempo.cpp
@@ -1,16 +1,24 @@
 #include "Tempo.hpp"
 
 Tempo::Tempo(int dia, int hora, int min, int seg) {
-    this->dia = dia;
-    this->hora = hora;
-    this->min = min;
-    this->seg = seg;
+  this->dia = dia;
+  this->hora = hora;
+  this->min = min;
+  this->seg = seg;
 
-    this->verificaConsistencia();
+  this->verificaConsistencia();
 }
 
-void verificaConsistencia() {
 
+
+void Tempo::verificaConsistencia() {}
+
+void Tempo::verificaSegundos() {
+    if (this->seg >= 60){
+        this->seg -= 60;
+        this->incrementaMinuto();
+    }
+    verificaMinutos();
 }
 
 Tempo::~Tempo() {}
